47c4cec2dad7b25c2b8c5d3819508b8181c9b469
Swaroop <breadfan18@gmail.com>
2021-01-30 21:39:45 -0500

    added event listener to submit button

--- landingScript.js
+++ landingScript.js
@@ -1,3 +1,23 @@
 const userName = "breadfan18";
 
-const userNameField =
+const userNameField = document.querySelector("#userNameField");
+const pwdField = document.querySelector("#pwdField");
+const submitButton = document.querySelector("#loginSubmit");
+const loginSection = document.querySelector(".loginSection");
+
+
+function login() {
+    let userNameEntered = userNameField.value;
+    let pwdEntered = pwdField.value;
+
+    if (userNameEntered !== userName) {
+        let message = document.createElement("div");
+        message.innerHTML = "<p>Wrong Username</p>";
+        message.setAttribute("class", "error");
+        console.log(message);
+
+        loginSection.appendChild(message);
+    }
+}
+
+submitButton.addEventListener("click", login, false);
