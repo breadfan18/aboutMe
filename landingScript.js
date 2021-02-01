c46a6caebe869a65e161da2f5474d81050a3cd94
Swaroop <breadfan18@gmail.com>
2021-02-01 18:23:19 -0500

    messing around with login credentials

--- landingScript.js
+++ landingScript.js
@@ -1,23 +1,36 @@
+/*
+* Ideas
+* - Create a show/hide pwd toggle
+* - Create Account Feature - save as objects in an array?? think about how to store
+* -
+* */
+
+
+
 const userName = "breadfan18";
-const pwd = "Test1234#";
+const pwd = "test";
 
 const userNameField = document.querySelector("#userNameField");
 const pwdField = document.querySelector("#pwdField");
 const submitButton = document.querySelector("#loginSubmit");
 const loginSection = document.querySelector(".loginSection");
 
+let wrongUserNameMessageCreated = false;
 
 function login() {
     let userNameEntered = userNameField.value;
     let pwdEntered = pwdField.value;
+    let errorMessage = document.createElement("p");
+    errorMessage.setAttribute("class", "error");
 
-    if (userNameEntered !== userName) {
-        let message = document.createElement("div");
-        message.innerHTML = "<p>Wrong Username</p>";
-        message.setAttribute("class", "error");
-        console.log(message);
-
-        loginSection.appendChild(message);
+    if (userNameEntered !== userName && !wrongUserNameMessageCreated) {
+        errorMessage.innerHTML = "Wrong Username";
+        loginSection.appendChild(errorMessage);
+        wrongUserNameMessageCreated = true;
+    }else if (userNameEntered === userName && pwdEntered !== pwd) {
+        errorMessage.innerHTML = "Wrong Password";
+        loginSection.appendChild(errorMessage);
+        wrongUserNameMessageCreated = true;
     }
 }
 
