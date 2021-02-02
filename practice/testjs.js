ce48c6eebd923083ca8eebc3725c61ce0b7fcf9b
Swaroop <breadfan18@gmail.com>
2021-02-02 14:27:11 -0500

    conditional framework for lapis papyrus is done

--- practice/testjs.js
+++ practice/testjs.js
@@ -6,33 +6,31 @@ let computer = {
     currentChoice: null
 };
 
-const choices = ["lapis", "papyrus", "scalpellus"];
+const choices = ["Lapis", "Papyrus", "Scalpellus"];
 let rand = Math.floor(Math.random() * 3);
 
 computer.currentChoice = choices[rand];
-console.log(computer.currentChoice);
 player.currentChoice = choices[1];
-console.log(player.currentChoice);
 
 if (computer.currentChoice === player.currentChoice) {
-    console.log("It's a tie!");
+    console.log("It's a tie! Both Computer and Player chose " + computer.currentChoice);
 }else if (computer.currentChoice === choices[0]) {
     if (player.currentChoice === choices[1]) {
-        console.log("Player wins!");
+        console.log("Player wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }else if (player.currentChoice === choices[2]) {
-        console.log("Computer wins!");
+        console.log("Computer wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }
 }else if (computer.currentChoice === choices[1]) {
     if (player.currentChoice === choices[0]) {
-        console.log("Player wins!");
+        console.log("Computer wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }else if (player.currentChoice === choices[2]) {
-        console.log("Computer wins!");
+        console.log("Player wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }
 }else if (computer.currentChoice === choices[2]) {
     if (player.currentChoice === choices[0]) {
-        console.log("Player wins!");
+        console.log("Player wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }else if (player.currentChoice === choices[1]) {
-        console.log("Computer wins!");
+        console.log("Computer wins! Computer chose " + computer.currentChoice + " and player chose " + player.currentChoice);
     }
 }
 
